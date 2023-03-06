118c54441f4017a5ead699ebd4f60e9c7dbef334
Abialidr <luckybelt79@gmail.com>
2023-03-06 14:48:30 +0530

    added more data

--- redux-essentials-example-app/src/features/posts/PostsList.js
+++ redux-essentials-example-app/src/features/posts/PostsList.js
@@ -1,18 +1,34 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { PostAuthor } from './PostAuthor'
+import { ReactionButtons } from './ReactionButtons'
+import { TimeAgo } from './TimeAgo'
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date))
 
-  const renderedPosts = posts.map((post) => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>{post.title}</h3>
-      <p className="post-content">{post.content}</p>
-      <Link to={`/posts/${post.id}`} className="button muted-button">
-        VIew post
-      </Link>
-    </article>
-  ))
+  const renderedPosts = orderedPosts.map((post) => {
+    console.log("🚀 ~ file: PostsList.js:19 ~ renderedPosts ~ post.userId:", post.userId)
+
+    return (
+      <article className="post-excerpt" key={post.id}>
+        <h3>{post.title}</h3>
+        <div>
+          <PostAuthor userId={post.userId} />
+          <TimeAgo timestamp={post.date} />
+        </div>
+        <p className="post-content">{post.content.substring(0, 100)}</p>
+
+        <ReactionButtons post={post} />
+        <Link to={`/posts/${post.id}`} className="button muted-button">
+          View Post
+        </Link>
+      </article>
+    )
+  })
 
   return (
     <>
--- redux-essentials-example-app/src/features/posts/postsSlice.js
+++ redux-essentials-example-app/src/features/posts/postsSlice.js
@@ -1,13 +1,23 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
-
+import { sub } from 'date-fns'
 const initialState = [
   {
     id: '1',
     title: 'First Post!',
     content:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.  ',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    userId: '0',
+    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
   },
-  { id: '2', title: 'Second Post', content: 'More text' },
+  {
+    id: '2',
+    title: 'Second Post',
+    content: 'More text',
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    userId: '1',
+    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
+  },
 ]
 
 const postsSlice = createSlice({
@@ -16,12 +26,7 @@ const postsSlice = createSlice({
   reducers: {
     addPost: {
       reducer: (state, action) => {
-        state.push({
-          id: action.payload.id,
-          title: action.payload.title,
-          content: action.payload.content,
-          userId: action.payload.userId,
-        })
+        state.push(action.payload)
       },
       prepare: (content, title, userId) => {
         return {
@@ -30,6 +35,8 @@ const postsSlice = createSlice({
             content,
             title,
             userId,
+            date: new Date().toISOString(),
+            reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
           },
         }
       },
@@ -42,8 +49,15 @@ const postsSlice = createSlice({
         existingPost.content = content
       }
     },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find((post) => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
+    },
   },
 })
 
 export default postsSlice.reducer
-export const { addPost, postUpdated } = postsSlice.actions
+export const { addPost, postUpdated, reactionAdded } = postsSlice.actions
